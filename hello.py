49e24eff6891a574cae752787c23fe585ab3ec70
blarneyosullivanbloggs <blarneyosullivanbloggs@gmail.com>
2016-04-26 15:34:05 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+# needed for cloud 9
+import os
+
+
+#-----------------------
+from flask import Flask
+app = Flask(__name__)
+
+#route
+@app.route('/')
+def hello_world():
+    return 'hello world!'
+    
+# being called from terminal
+#.run creates a waiting server
+if __name__ == '__main__':
+    host = os.getenv('IP', '0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
+    app.run(host=host, port=port)
+    
+    
+    
+    
